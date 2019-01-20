9d5abb5b891b9bc8b5d8d1eb2e0a428cbb99b070
Michael Kipchumba <michaelchumba09@gmail.com>
2019-01-20 16:44:42 +0300

    create user component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
